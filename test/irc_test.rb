ceae98a3fe6e3288fce0c4da257040f454ea58bc
Renne Nissinen <rennex@iki.fi>
2020-03-08 10:44:45 +0200

    Use a before block in tests

--- test/irc_test.rb
+++ test/irc_test.rb
@@ -3,43 +3,44 @@ require "minitest/autorun"
 require_relative "../irc.rb"
 
 describe Hellbender::IRC do
+  before do
+    @irc = Hellbender::IRC.new
+    # suppress log printing
+    @irc.log.level = Logger::FATAL
+  end
+
   it "remembers its config hash" do
     irc = Hellbender::IRC.new({foo: "bar"})
     assert_equal "bar", irc.config[:foo]
   end
 
   it "parses server messages" do
-    irc = Hellbender::IRC.new
     assert_equal [":Nick!user@ser.ver", "PRIVMSG", ["#channel", "hello, world!"]],
-                  irc.parse_msg(":Nick!user@ser.ver PRIVMSG #channel :hello, world!")
-    assert_equal [nil, "PRIVMSG", ["bar", "hello"]], irc.parse_msg("PRIVMSG bar :hello")
-    assert_equal [nil, "CMD", ["x", ""]], irc.parse_msg("CMD x :")
-    assert_equal [":ser.ver", "CMD", ["x", ""]], irc.parse_msg(":ser.ver CMD x :")
-    assert_equal [nil, "PING", ["123 45"]], irc.parse_msg("PING :123 45")
+                  @irc.parse_msg(":Nick!user@ser.ver PRIVMSG #channel :hello, world!")
+    assert_equal [nil, "PRIVMSG", ["bar", "hello"]], @irc.parse_msg("PRIVMSG bar :hello")
+    assert_equal [nil, "CMD", ["x", ""]], @irc.parse_msg("CMD x :")
+    assert_equal [":ser.ver", "CMD", ["x", ""]], @irc.parse_msg(":ser.ver CMD x :")
+    assert_equal [nil, "PING", ["123 45"]], @irc.parse_msg("PING :123 45")
   end
 
   it "guesses message encodings and converts to UTF-8" do
-    irc = Hellbender::IRC.new
     # emulate raw messages received from a TCP socket
     raw1 = "föö bär".encode("UTF-8").force_encoding("ASCII-8BIT")
     raw2 = "föö bär".encode("ISO-8859-1").force_encoding("ASCII-8BIT")
     refute_equal raw1, raw2
     [raw1, raw2].each do |raw|
-      irc.guess_encoding(raw)
+      @irc.guess_encoding(raw)
       assert_equal "föö bär", raw
       assert_equal "UTF-8", raw.encoding.to_s
     end
   end
 
   it "processes server messages" do
-    irc = Hellbender::IRC.new
     q = Queue.new
-    irc.add_listener(q)
-    # suppress log printing
-    irc.log.level = Logger::FATAL
+    @irc.add_listener(q)
     message = [":ser.ver", "PRIVMSG", ["you", "hello"]]
 
-    irc.process_msg(*message, "should not see this")
+    @irc.process_msg(*message, "should not see this")
     assert_equal 1, q.size
     r = q.pop
     assert_equal message, r
@@ -49,10 +50,10 @@ describe Hellbender::IRC do
     refute_same message[2][0], r[2][0]
     refute_same message[2][1], r[2][1]
 
-    # check ping replies while we're at it
+    # check ping replies
     ponged = nil
-    irc.stub(:sendraw, proc {|msg| ponged = msg }) {
-      irc.process_msg(nil, "PING", ["foo"])
+    @irc.stub(:sendraw, proc {|msg| ponged = msg }) {
+      @irc.process_msg(nil, "PING", ["foo"])
     }
     assert_equal "PONG foo", ponged
     assert_equal 0, q.size
